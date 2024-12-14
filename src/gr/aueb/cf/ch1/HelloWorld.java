c12e97490d6f4ae77cd797f5517c1786e5b51205
Vagis Kostas <vagis.kwn@outlook.com>
2024-12-14 17:29:01 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,15 @@
+package gr.aueb.cf.ch1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints Hello Coding Factory
+
+        /*
+         * Prints in standard output
+         * "Hello Coding Factory"
+         */
+        System.out.println("Hello Coding Factory!");    // Prints Hello Coding Factory
+        System.out.print("Hello World!");
+        System.out.println();
+    }
+}
